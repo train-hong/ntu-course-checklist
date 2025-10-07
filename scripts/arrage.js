73df96ebc283b1ce2493b2926f6a3f4aa8f60956
dancingactor <s990346@gmail.com>
2025-10-07 18:43:04 +0800

    feat: new constant and some funciton

--- scripts/arrage.js
+++ scripts/arrage.js
@@ -1,10 +1,73 @@
-const DEPARTMENT_REQUIRED_CREDITS = 51;  // 系定必修學分
+/*
+courses = { "共同必修課程": [{
+    "name": "國文（一）",
+    "credit": 3, 
+    "semester": "112/1"
+}, {
+    "name": "英文",
+    "credit": 3, 
+    "semester": "112/1"
+}], 
+"通識": [{
+    "name": "某甲通識",
+    "credit": 3, 
+    "semester": "112/1",
+    "scope": [2, 5],
+    "star": true
+}, {
+    "name": "某乙通識",
+    "credit": 3, 
+    "semester": "112/1",
+    "scope": [3], 
+    "star": false
+}]}
+*/
+
 const TOTAL_SELECTIVE_CREDITS = 53       // 選修總學分
+const DEPARTMENT_REQUIRED_CREDITS = 51;  // 系定必修學分
 const DEPARTMENT_SELECTIVE_CREDITS = 21; // 系內選修學分
 const COLLEGE_SELECTIVE_CREDITS = 9;     // 院內選修學分
 const GEN_CREDITS = 15;                  // 通識學分
 const GEN_SCOPE_NUM = 3;
 const GEN_SCOPES = new Set([1, 2, 3, 5, 8]);
+
+function arrange(courses) {
+    
+    // 體育、共同必修不用處理
+    // 一般選修、通識
+    // 系必修、院選修、系選修以溢出處理即可
+    // 我覺得要先處理系必修、院選修、系選修、一般選修，因為通識課程是否要移動到一般選修取決於一般選修的學分數
+    
+    // handle 系必修、院選修、系選修、一般選修
+    if (courses["系定必修"].length > DEPARTMENT_REQUIRED_CREDITS) {
+      
+    }
+
+    // handle 通識
+    generalResults = fulfilGeneral(courses["通識"]);
+    
+    if (generalResults["fulfil"] == true) {
+        
+    } 
+    
+    
+    
+    return arranged_courses
+}
+
+function remain(arranged_courses) {
+    // return courses, number of credits to take
+
+/*
+number of credits to take = { "共同必修課程": 3, "通識": {
+    "credit remaining": 6, 
+    "number of scopes type remaining": 2
+    "scopes lack": ["1", "2", "3", "5"]
+}, ...}
+*/
+    
+}
+
 
 /**
  * @param {Object<string, Set<number>>} generalCourses - 
@@ -54,4 +117,3 @@ function fulfilGeneral(generalCourses) {
     return { fulfil: false, needScope: missing };
   }
 }
-
